6159a341a6fd43cf348252db986a31d3398c8dba
lucia33 <liu_chang2010@hotmail.com>
2018-09-16 11:56:37 -0400

    Revert "Add footer"
    This reverts commit 71fc885b57406540904c50754bcb7c3e91dcfe7f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CollapseModule } from 'ngx-bootstrap';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    CollapseModule,
-    AngularFontAwesomeModule
+    CollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
