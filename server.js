3463bc98f92b0d05d8d43d7e24eb7f912b5639fe
lucia33 <liu_chang2010@hotmail.com>
2018-09-19 00:37:33 -0400

    Fix deploy issue
    Fix deploy issue

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/changliu-ca'));
+app.use(express.static(__dirname + '/dist/changliuCA'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/changliu-ca/index.html'));
+res.sendFile(path.join(__dirname+'/dist/changliuCA/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
